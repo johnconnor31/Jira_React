6a0767a3582c1cc151e8053f1d6254f954cd1ba8
sairam s <sairamsingireesu@gmail.com>
2020-10-03 01:42:01 +0530

    Added social media icons for signup

--- src/components/loginDialog.js
+++ src/components/loginDialog.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Dialog, DialogTitle,TextField, DialogContent, Button } from '@material-ui/core';
+import { Dialog, DialogTitle,TextField, DialogContent, Button, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { Facebook, Twitter, Reddit } from '@material-ui/icons';
 
 const useStyles = makeStyles({
     paper: {
         width: '40%',
-        height: '50%',
+        height: '60%',
         display:'flex',
         alignItems:'center'
     },
@@ -13,6 +14,18 @@ const useStyles = makeStyles({
         '&>*': {
             margin: '3% 4% 0 0'
         }
+    },
+    loginIcons: {
+        display: 'flex',
+        justifyContent: 'space-evenly'
+    },
+    fb: {
+        color:'blue'
+    },
+    twitter: {
+        color:'cornflowerblue'
+    },reddit: {
+        color:'red'
     }
 });
 export default function login(props){
@@ -32,6 +45,11 @@ export default function login(props){
                 label='Password' />
                 <Button variant='contained' color='primary'>Login</Button>
                 <Button variant='contained'>Signup</Button>
+                <div className={classes.loginIcons}>
+                    <IconButton><Facebook fontSize='large' className={classes.fb} /></IconButton>
+                    <IconButton><Twitter fontSize='large' className={classes.twitter} /></IconButton>
+                    <IconButton><Reddit fontSize='large' className={classes.reddit} /></IconButton>
+                </div>
             </DialogContent>
         </Dialog>
     );
